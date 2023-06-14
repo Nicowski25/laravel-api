21a22e66df9b65694926948e1ef4f8183d9464af
Nicowski25 <nicola.faedo0610@gmail.com>
2023-06-14 18:23:18 +0200

    add index projectController

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index() 
+    {
+        $projects = Project::orderByDesc('id')->get();
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects,
+        ]);
+    }
 }
